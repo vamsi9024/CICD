c9860428d353d3a0869d4daeb8dcd3be3ece16d0
vamsi9024 <petavamsikrishna9024@gmail.com>
2025-11-06 20:43:20 +0530

    commited

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot CI/CD!";
+        return "Hello from Spring Boot CI/CD! ";
     }
 }
